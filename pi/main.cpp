7ee4cdd1c1e145903d9e866e4c0d6b9e98e2d965
Felipe Teles <felipersteles@gmail.com>
2024-04-15 22:30:52 -0300

    Initial Commit

---
+++ pi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pi
+//
+//  Created by Felipe Teles on 15/04/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
