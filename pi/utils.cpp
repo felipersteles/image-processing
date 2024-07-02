ebdf0e64dadf5e9833b5a73b84fd87f33fe0f86d
Felipe Teles <felipersteles@gmail.com>
2024-07-02 18:53:11 -0300

    add: storage results in an object

---
+++ pi/utils.cpp
@@ -0,0 +1,8 @@
+//
+//  utils.cpp
+//  pi
+//
+//  Created by Felipe Teles on 02/07/24.
+//
+
+#include "utils.hpp"
---
+++ pi/utils.hpp
@@ -0,0 +1,13 @@
+//
+//  utils.hpp
+//  pi
+//
+//  Created by Felipe Teles on 02/07/24.
+//
+
+#ifndef utils_hpp
+#define utils_hpp
+
+#include <stdio.h>
+
+#endif /* utils_hpp */
